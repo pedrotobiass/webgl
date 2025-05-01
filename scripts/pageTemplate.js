5ee1d2f1653472aa82bec84e4c8648db6f76c5c1
pedrotobiass <pedrotobiassilvadejesus@gmail.com>
2025-05-01 14:31:48 -0300

    template page

--- scripts/pageTemplate.js
+++ scripts/pageTemplate.js
@@ -1,21 +1,57 @@
-import { handleError } from "./errorHandler.js"
+import { handleError } from "./errorHandler.js";
 
-function emitTemplateSucess(data) {
-    const event = new CustomEvent('templateLoaded', {
-        data
-    })
+class PageTemplateManager {
+    construtor() {
+        this.canvas = null
+        this.gl = null
+    }
 
-    document.dispatchEvent(event)
+    createCanvas() {
+        const element = document.createElement('canvas')
+        element.width = 512
+        element.height = 512
+        element.id = "maincanvas"
+
+        document.body.appendChild(element)
+
+        this.canvas = element
+    }
+
+    createStyles() {
+        const element = document.createElement('link')
+        element.rel = "stylesheet"
+        element.href = "./styles.css"
+
+        document.head.appendChild(element)
+    }
+
+    createGL() {
+        const gl = this.canvas.getContext('webgl2')
+        this.gl = gl
+    }
+
+    emitTemplateLoaded() {
+        const event = new CustomEvent("templateLoaded", {
+            gl: this.gl,
+        });
+
+        document.dispatchEvent(event);
+    }
 }
 
 async function main() {
-    alert('Hello, World!')
+    const manager = new PageTemplateManager()
+    manager.createCanvas()
+    manager.createStyles()
+    manager.createGL()
+
+    manager.emitTemplateLoaded()
 }
 
 try {
-    window.addEventListener('DOMContentLoaded', async() => {
-        await main()
-    })
-} catch(error) {
-    handleError(error)
+    window.addEventListener("DOMContentLoaded", async () => {
+        await main();
+    });
+} catch (error) {
+    handleError(error);
 }
